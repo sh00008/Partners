a81b3d11d5b54ddaab17b7aaf3299140205558e4
sh00008 <sh00008@sohu.com>
2013-06-13 21:57:53 +0800

    更新libisaybio库。

---
+++ Classes/Favor/Listening/RecordingObject.h
@@ -0,0 +1,13 @@
+//
+//  RecordingObject.h
+//  Partners
+//
+//  Created by JiaLi on 13-6-13.
+//  Copyright (c) 2013年 JiaLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RecordingObject : NSObject
+
+@end
---
+++ Classes/Favor/Listening/RecordingObject.mm
@@ -0,0 +1,13 @@
+//
+//  RecordingObject.m
+//  Partners
+//
+//  Created by JiaLi on 13-6-13.
+//  Copyright (c) 2013年 JiaLi. All rights reserved.
+//
+
+#import "RecordingObject.h"
+
+@implementation RecordingObject
+
+@end
